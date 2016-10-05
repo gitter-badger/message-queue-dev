31efeb5680f9da045a247e8f47ea5c6c07adfaeb
Alexander Kozienko <pascalamsg@gmail.com>
2016-10-05 12:47:21 +0300

    remove dbal transport

--- Tests/Unit/Client/DriverFactoryTest.php
+++ Tests/Unit/Client/DriverFactoryTest.php
@@ -1,14 +1,10 @@
 <?php
 namespace FormaPro\MessageQueue\Tests\Unit\Client;
 
-use Doctrine\DBAL\Connection;
 use FormaPro\MessageQueue\Client\Config;
-use FormaPro\MessageQueue\Client\DbalDriver;
 use FormaPro\MessageQueue\Client\DriverFactory;
 use FormaPro\MessageQueue\Client\NullDriver;
 use FormaPro\MessageQueue\Transport\ConnectionInterface;
-use FormaPro\MessageQueue\Transport\Dbal\DbalConnection;
-use FormaPro\MessageQueue\Transport\Dbal\DbalSession;
 use FormaPro\MessageQueue\Transport\Null\NullConnection;
 use FormaPro\MessageQueue\Transport\Null\NullSession;
 
@@ -24,21 +20,6 @@ class DriverFactoryTest extends \PHPUnit_Framework_TestCase
 
         self::assertInstanceOf(NullDriver::class, $driver);
         self::assertAttributeInstanceOf(NullSession::class, 'session', $driver);
-        self::assertAttributeSame($config, 'config', $driver);
-    }
-
-    public function testShouldCreateDbalSessionInstance()
-    {
-        $config = new Config('', '', '', '');
-
-        $doctrineConnection = $this->createMock(Connection::class);
-        $connection = new DbalConnection($doctrineConnection, 'aTableName');
-
-        $factory = new DriverFactory([DbalConnection::class => DbalDriver::class]);
-        $driver = $factory->create($connection, $config);
-
-        self::assertInstanceOf(DbalDriver::class, $driver);
-        self::assertAttributeInstanceOf(DbalSession::class, 'session', $driver);
         self::assertAttributeSame($config, 'config', $driver);
     }
 
