0feaad6f6927f6d63580817df41c63f5cfb3c99b
Alexander Kozienko <pascalamsg@gmail.com>
2016-11-16 16:37:11 +0200

    topics and destionations functional tests

--- pkg/mq-bundle/Tests/Functional/DestinationsCommandTest.php
+++ pkg/mq-bundle/Tests/Functional/DestinationsCommandTest.php
@@ -2,6 +2,7 @@
 namespace Formapro\MessageQueueBundle\Tests\Functional;
 
 use Formapro\MessageQueue\Client\Meta\DestinationsCommand;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class DestinationsCommandTest extends WebTestCase
 {
@@ -10,5 +11,19 @@ class DestinationsCommandTest extends WebTestCase
         $command = $this->container->get('formapro_message_queue.client.meta.destinations_command');
 
         $this->assertInstanceOf(DestinationsCommand::class, $command);
+    }
+
+    public function testShouldDisplayRegisteredDestionations()
+    {
+        $command = $this->container->get('formapro_message_queue.client.meta.destinations_command');
+
+        $tester = new CommandTester($command);
+        $tester->execute([]);
+
+        $display = $tester->getDisplay();
+
+        $this->assertContains(' default ', $display);
+        $this->assertContains('formapro.default', $display);
+        $this->assertContains('formapro_message_queue.client.route_message_processor', $display);
     }
 }
