77c74a912ca2557a4e6b0add6c4085c265bc1281
Maksym Kotliar <kotlyar.maksim@gmail.com>
2016-11-24 12:40:59 +0200

    restore lost amqp driver and amqp transport factory.

--- pkg/mq-amqp-ext/AmqpMessage.php
+++ pkg/mq-amqp-ext/AmqpMessage.php
@@ -5,6 +5,9 @@ use Formapro\Jms\Message;
 
 class AmqpMessage implements Message
 {
+    const DELIVERY_MODE_NON_PERSISTENT = 1;
+    const DELIVERY_MODE_PERSISTENT = 2;
+
     /**
      * @var string
      */
