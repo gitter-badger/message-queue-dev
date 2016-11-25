7eb657f7fde41ebbd4ac8790fc08a5702fff1149
Maksym Kotliar <kotlyar.maksim@gmail.com>
2016-11-25 16:26:54 +0200

    upd split script. use github repos.

--- bin/subtree-split.sh
+++ bin/subtree-split.sh
@@ -18,16 +18,18 @@ function remote()
     git remote add $1 $2 || true
 }
 
-remote fms git@gitlab.com:askozienko/FMS.git
-remote mq git@gitlab.com:askozienko/MessageQueue.git
-remote stomp git@gitlab.com:askozienko/Stomp.git
-remote bundle git@gitlab.com:askozienko/MessageQueueBundle.git
-remote job-queue git@gitlab.com:askozienko/JobQueue.git
-remote test git@gitlab.com:askozienko/MessageQueueTest.git
+remote fms git@github.com:formapro/fms.git
+remote mq git@github.com:formapro/message-queue.git
+remote stomp git@github.com:formapro/stomp-transport.git
+remote amqp-ext git@github.com:formapro/amqp-ext-transport.git
+remote bundle git@github.com:formapro/message-queue-bundle.git
+remote job-queue git@github.com:formapro/job-queue.git
+remote test git@github.com:formapro/message-queue-test.git
 
 split 'pkg/fms' fms
 split 'pkg/mq' mq
 split 'pkg/mq-stomp' stomp
+split 'pkg/mq-amqp-ext' amqp-ext
 split 'pkg/mq-bundle' bundle
 split 'pkg/job-queue' job-queue
 split 'pkg/test' test
